3945d1edfec62498ac2262b2fd6ccdd6a3b04012
Olga Semenova <helen597597@gmail.com>
2023-11-23 11:46:07 +0500

    add class Keyboard, tests

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,8 +2,16 @@ from src.item import Item
 
 
 class Mixin:
+    """Класс-миксин"""
 
     def __init__(self, name, price, quantity, language):
+        """Инициализация
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        :param language: Раскладка клавиатуры.
+        """
         super().__init__(name, price, quantity)
         self.__language = language
 
@@ -16,6 +24,9 @@ class Mixin:
     #     self.__language = language
 
     def change_lang(self):
+        """
+        Меняет раскладку клавиатуры
+        """
         if self.__language == "EN":
             self.__language = "RU"
         else:
@@ -24,6 +35,8 @@ class Mixin:
 
 
 class Keyboard(Mixin, Item):
+    """Класс для товара 'Клавиатура'"""
 
     def __init__(self, name, price, quantity, language="EN"):
+        """Создание экземпляра класса клавиатура"""
         super().__init__(name, price, quantity, language)
